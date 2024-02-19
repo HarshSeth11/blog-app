39a76b3e139064317541e25d72cab116c108bdce
HarshSeth11 <harshsethr@gmail.com>
2024-02-19 19:18:02 +0530

    Wrote routers of post routes

--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -7,4 +7,12 @@ const {upload} = require("../middleware/multer.middleware");
 
 router.post('/createPost', upload.single("thumbnail") ,postController.createBlog_Post);
 
+router.get('/all-posts', postController.getAllPosts);
+
+router.patch('/update-post/:postId', postController.updatePostDetails);
+
+router.patch('/change-thumbnail/:postId', upload.single("thumbnail"), postController.changeThumbnail);
+
+router.delete('/delete-post/:postId', postController.deletePost);
+
 module.exports = router;
